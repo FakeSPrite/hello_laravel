11567cec4fbd33c3b1768bf6e023737addd12ad1
vagrant <vagrant@homestead>
2018-06-17 07:52:42 +0000

    Profile for Heroku

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -79,7 +79,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Laravel Anvis
+                    Laravel sss
                 </div>
 
                 <div class="links">
